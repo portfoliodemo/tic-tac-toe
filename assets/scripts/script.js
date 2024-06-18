7e00c9bca5d904fee99ae23203634f634ad28067
Nathan Ryan <rockymountainshredder@gmail.com>
2024-06-18 00:21:21 -0600

    Added Gameboard IIFE

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,7 @@
 
 // Tic-Tac-Toe
 
-// Create a gameBoard object, using an IIFE
-// Immediately Invoked Function Expression
+// Create a gameBoard object, using an IIFE (Immediately Invoked Function Expression)
 // The game board displays the tic-tac-toe board
 const Gameboard = (() => {
     let board = ['', '', '', '', '', '', '', '', ''];
@@ -28,37 +27,40 @@ const Gameboard = (() => {
     return { getBoard, resetBoard, placeMarker };
 
 })();
+ 
+// Update the DOM
+// TODO: Add currentPlayer instead of only 'X'
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach(cell => {
+    cell.addEventListener('click', (e) => {
+        const index = e.target.dataset.index;
+        if (Gameboard.placeMarker(index, 'X')) {
+            updateBoard();
+        }
+    });
+});
+
+// Update the Game Board
+function updateBoard() {
+    const board = Gameboard.getBoard();
+    board.forEach((marker, index) => {
+        cells[index].innerText = marker;
+    });
+}
 
 // const gameBoard = {
 //     board: ['', '', '', '', '', '', '', '', ''],
 // };
 
 // Create a player object
-const player = {
-    playerOneName: "",
-    playerOneMarker: "",
-    playerTwoName: "",
-    playerTwoMarker: "",
-};
-
-// Create a player/player(s) object
-// Player Factory
-// const Player = (name, symbol) => {
-//     const getSymbol = () => symbol;
-//     return { name, getSymbol };
+// const player = {
+//     playerOneName: "",
+//     playerOneMarker: "",
+//     playerTwoName: "",
+//     playerTwoMarker: "",
 // };
 
 
-// Create a game/flow object
 
-// What happens in a tic-tac-toe game?
-
-// An empty board is displayed initially..
-
-// How should that board be displayed? 9 buttons? 
-
-// Command line only, or with an interface? 
-
-// This is meant to be a webpage so an interface seems reasonable
-// The command line idea is cool too, and eliminates the need for a UI
 
